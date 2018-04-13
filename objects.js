06461fc34ccb110e842810f586f649895f300141
John Raber <jrab224@gmail.com>
2018-04-13 01:52:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { flor: 'rely' }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle
+  playlist[artistName] = songTitle
   return playlist
 }
